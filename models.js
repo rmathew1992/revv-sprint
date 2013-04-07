27083132584a568aad99cc76774c9dbdddec8166
mkorpusik <mkorpusik@ubuntu.(none)>
2013-04-06 20:32:39 -0400

    saves form info to db

--- models.js
+++ models.js
@@ -1,14 +1,22 @@
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/goalplanner');
 
+var taskSchema = mongoose.Schema({
+  task: String,
+  duration: String
+});
+
+var Task = mongoose.model('Task', taskSchema);
+
 var goalSchema = mongoose.Schema({
   goal: String,
-  tasks: [String],
-  daily: [String],
-  start: [Number],
-  end: [Number]
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+  daily: String,
+  start: [String],
+  end: [String]
 });
 
 var Goal = mongoose.model('Goal', goalSchema);
 
 module.exports.Goal = Goal;
+module.exports.Task = Task;
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@
 // Mondodb variables
 var models = require('../models');
 var Goal = models.Goal;
+var Task = models.Task;
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
@@ -17,6 +18,27 @@ exports.intermediate = function(req, res){
 };
 
 exports.newgoal = function(req, res){
-  console.log(req.body);
+  var goal_info = req.body;
+
+  // form has been submitted (as opposed to "new goal" button pressed)
+  if (Object.keys(goal_info).length > 0) {
+
+	// save goal info to mongodb
+	var goal = goal_info.Goal;
+	var task = new Task({ task: goal_info.Task, duration: goal_info.Duration});
+    task.save(function (err) {
+      if (err)
+        return console.log(err);
+    });
+	var tasks = [task];
+	var start = [goal_info.startdateday, goal_info.startdatemonth, goal_info.startdateyear];
+	var end = [goal_info.enddateday, goal_info.enddatemonth, goal_info.enddateyear];
+    var new_goal = new Goal({ goal: goal, tasks: tasks, daily: goal_info.Daily, start: start, end: end});
+    new_goal.save(function (err) {
+      if (err)
+        return console.log(err);
+    });
+  }
+
   res.render('newgoal', { title: 'Make a New goal!' });
 };
